2fc6e1f202fd4d84c9eabb1e85421b0396e2833f
Salma Abdirahman <salmaaaxx@outlook.com>
2025-04-18 12:21:23 +0100

    CREATE: template html files EDIT: changed stylesheet to pink theme

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,10 +1,70 @@
 # from flask import Flask,render_template
 from app import app  # This pulls in the app instance
-from flask import render_template
+from flask import render_template, request
 from flask_app import app
 
 
-# def register_routes():
+# define routes():
+@app.route('/')
 @app.route('/home')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
+
+
+@app.route('/membership-plans')
+def membership_plans():
+    return render_template('membership_plans.html')
+
+
+@app.route('/trainers')
+def trainers():
+    return render_template('trainers.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    # You can customize this later to actually do something
+    return f"<h2>Search Results for: <em>{query}</em></h2>"
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# # from flask import Flask,render_template
+# from app import app  # This pulls in the app instance
+# from flask import render_template
+# from flask_app import app
+# import os
+#
+#
+# # def register_routes():
+# @app.route('/home')
+# def home():
+#     return render_template("index.html")
